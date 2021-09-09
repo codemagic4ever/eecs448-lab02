0fd864903ab5631b9e5fff5e8b120207c44fe4cd
Jui <jui.nagarkar@ku.edu>
2021-09-08 20:35:27 -0500

    commiting the changes made to the size method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -46,20 +46,7 @@ bool LinkedList<T>::search(T value) const
 		Fix this method
 	*/
 
-	while(temp != nullptr)
-	{
-	if(temp -> getValue() == value)  //checking if the temp pointing to getValue() is equal to value
-	 //if it does then the we can continue to say that isFound = true
-	{
-		isFound = true;
-	       	//if the if condition is met then the bool isFound will be true 	
-		//and we will return isFound. If the if condition is not met
-		//then it will be false
-		return(isFound);
 
-	}
-	temp = temp-> getNext(); //here setting temp equal to the value of temp pointing to getNext(). Getting the value by getNext()
-	}
 	return(isFound);
 }
 
@@ -121,24 +108,6 @@ bool LinkedList<T>::removeBack()
 	/** TODO 
 		Fix this method
 	*/
-	if(!isEmpty()) //followed the method remove front as a guide
-		//based on my understanding from the discussion with my TA our goal is to remove back 
-		//which means that we have to traverse from start to get to the last node
-		//in order to do that we are required to change the remove front in order for us to have remove back
-        {
-
-                lastNode = m_front; //here instead of temp we are setting lastNode equal to m_front
-                while(lastNode -> getNext() != nullptr) //this is saying that the loop will run as long as the value is not equal to  null
-		{
-			secondintoLast = lastNode; //if the while condition is met we set secondintoLast equal to lastNode.
-			lastNode  = lastNode->getNext(); //then the lastNode is equal to the lastNode pointing to getNext() value. 
-                
-		}
-		delete lastNode; //here just like removeFront we will delete but instead of temp we are deleting lastNode
-		secondintoLast -> setNext(nullptr); 
-                m_size--; //same steps as removeFront
-                isRemoved = true;
-	}
 
 	return(isRemoved);
 }	
